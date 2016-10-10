aa1f4804de2af7089c80f2d53e4a3750efa382e6
Ferit Tunçer <ferit@cryptolab.net>
2016-10-10 20:25:34 +0300

    test eklendi, ftwa'da küçük değişiklikler

--- ftwa.py
+++ ftwa.py
@@ -12,18 +12,28 @@ from datetime import date
 class Person(object):
 	@staticmethod
 	def is_date(*args):
-  		return all(isinstance(arg, date) for arg in args) 
+  		return all(isinstance(arg, date) for arg in args)
+
+	@staticmethod
+	def is_person(*args):
+		return all(isinstance(arg, Person) for arg in args)
+	
+	@staticmethod
+	def is_none(*args):
+		return all(isinstance(None, (date)) for arg in args) 
 
 	def __init__(self, name=None, surname=None, gender=None, birthdate: date=None, deathdate: date=None, father=None, mother=None, *children):
 
 		#if not isinstance(deathdate, date) or not isinstance(birthdate, date):
 		#	raise TypeError
 		
-		if not Person.is_date(deathdate, birthdate):
-  			raise TypeError
+		#if not Person.is_date(deathdate, birthdate) and not None in(birthdate, deathdate):
+  		#	raise TypeError
 
-
-		#var_is_good = any(isinstance(var, t) for t in [type1, type2, type3])
+		#if not Person.is_person(father, mother, *children):
+  		#	raise TypeError
+	
+		#print("any:{}".format(all(isinstance(var, date) for var in [birthdate, deathdate])))
 
 		self.gender = gender
 		self.name = name
@@ -39,19 +49,26 @@ class Person(object):
 		temp_tuple = (person, )
 		self.children = self.children + temp_tuple
 
-	def add_father(self, person):
+	def set_father(self, person):
 		self.father = person
-	def add_mother(self, person):
+
+	def get_father(self):
+		return self.father
+
+	def set_mother(self, person):
 		self.mother = person
+
+	def get_mother(self):
+		return self.mother
+
 	def set_is_placeholder(self):
 		if self.gender:
 			self.is_placeholder = False
 		else:
 			self.is_placeholder = True
+	
 	def get_age(self):
 		return
-
-	
 
 	def str(self):
 		temp = "name={}, surn={}, g={}, bd={}, dd={}, f={}, m={}, p={}".format(self.name, self.surname, self.gender, self.birthdate, self.deathdate, self.father, self.mother, self.is_placeholder)
@@ -62,12 +79,12 @@ class Person(object):
 
 def main():
 	print ("test")
-	Veli = Person("veli", "deli", "male", date.today(), date.today(), None, None)
+	Veli = Person("veli", "deli", "male", date.today(), date.today(), "o", None)
 	Veli2 = Person("veli2", "deli", "male", date.today(), date.today(), None, None)
 	Ali = Person("ali", "deli", "male", date.today(), date.today(), None, None, Veli, Veli2)
 	Fitnat = Person("fitnat", "deli", "female", date.today(), date.today(), None, None, Veli, Veli2)
 
-	asda = Person(name="veledizina", deathdate="", birthdate=date.today())
+	asda = Person(name="veledizina")
 
 	print(asda.str())
 	print(asda.gender == True)
--- tests_ftwa.py
+++ tests_ftwa.py
@@ -4,10 +4,39 @@ import ftwa
 import unittest
 import os
 import re
+import time
+from datetime import date
 
 class Test(unittest.TestCase):
 
 	def test(self):
+
+		Veli = ftwa.Person("Veli", "Yanyatan", "male", date.today(), date.today(), None, None)		
+
+		Ali = ftwa.Person("Ali", "Yanyatan", "male", date.today(), date.today(), None, None, Veli)
+		Fitnat = ftwa.Person("Fitnat", "Yanyatan", "female", date.today(), date.today(), None, None, Veli)
+
+		
+		Deli = ftwa.Person("Deli", "Yanyatan", "male", date.today(), date.today(), Ali, Fitnat)
+
+		Ali.add_child(Deli)
+		Fitnat.add_child(Deli)
+		
+		Veli.set_father(Ali)
+		Veli.set_mother(Fitnat)
+
+		assert Veli.get_father() == Ali
+		assert Veli.get_mother() == Fitnat
+		assert Deli.get_father() == Ali
+		assert Deli.get_mother() == Fitnat
+		assert Ali.get_father() == None		
+		assert Ali.get_mother() == None
+		assert Fitnat.get_father() == None
+		assert Fitnat.get_mother() == None
+
+		Veledizina = ftwa.Person(name="Veledizina")
+
+
 		assert 1==1
 
 if __name__ == '__main__':
